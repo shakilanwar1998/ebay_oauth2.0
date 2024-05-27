fb2feeeb93521c0ef20f481253dee8fdf922be2f
Shakil Anwar <38348848+shakilanwar1998@users.noreply.github.com>
2024-05-27 08:13:27 +0600

    Route added

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/auth',[\App\Http\Controllers\AuthenticationController::class,'handleRedirect']);
+Route::get('/token',[\App\Http\Controllers\AuthenticationController::class,'getAccessToken']);
